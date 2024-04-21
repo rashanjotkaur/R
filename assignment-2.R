5bad155dcf06a80177472b37e59efe59b945cde5
rashanjotkaur <40189953+rashanjotkaur@users.noreply.github.com>
2024-04-21 15:46:40 +0530

    Create assignment-2.R

---
+++ assignment-2.R
@@ -0,0 +1,20 @@
+# Assignment 2
+
+# Exercise - 2.3
+
+# Problem 1
+n <- 100
+sumofn <- n*(n+1)/2
+print(sumofn)
+
+# Problem 2
+n_new <- 1000
+sumofn_new <- n_new*(n_new+1)/2
+print(sumofn_new)
+
+# Problem 4
+n1 <- sqrt(100)
+n2 <- log(n1,base = 10)
+print(n2)
+
+
