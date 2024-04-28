6484acb3e6818056f93a4666535325fb0e1f3e01
rashanjotkaur <40189953+rashanjotkaur@users.noreply.github.com>
2024-04-28 12:21:29 +0530

    Create solution-assignment-5.R

---
+++ solution-assignment-5.R
@@ -0,0 +1,11 @@
+# Assignment: Assessment of Vectors
+
+# Q1: 
+# Ans: C
+
+
+# Q2: Use R to compute log base 4 of 1024
+cost <- c(50, 75, 90, 100, 150)  
+food <- c("pizza", "burgers", "salads", "cheese", "pasta")  
+names(cost) <- food
+## Similarly, for city and temp
