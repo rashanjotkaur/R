c1c80dd5e683aa76df45da2fe0a78c7a928489d2
rashanjotkaur <40189953+rashanjotkaur@users.noreply.github.com>
2024-04-28 12:45:27 +0530

    Update solution-assignment-5.R

--- solution-assignment-5.R
+++ solution-assignment-5.R
@@ -1,11 +1,61 @@
 # Assignment: Assessment of Vectors
 
 # Q1: 
+x <- c(4,"seven", 9)
+x
 # Ans: C
 
+# Q2: Associate the temperature to its related city.
+temp <- c(23, 24, 56, 34, 54)
+city <- c("Boston", "LA", "California", "New York", "Penn")
+names(temp) <- city
+temp
+city
 
-# Q2: Use R to compute log base 4 of 1024
-cost <- c(50, 75, 90, 100, 150)  
-food <- c("pizza", "burgers", "salads", "cheese", "pasta")  
-names(cost) <- food
-## Similarly, for city and temp
+# Q3:
+temp <- c(35, 88, 42, 84, 81, 30)
+
+# Q4:
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+
+# Q5:
+names(temp) <- city
+temp
+
+# Q6: 
+temp[1:3]
+
+# Q7:
+temp[c(3,5)]
+
+# Q8:
+12:73
+
+# Q9:
+x <- seq(1,100,2)
+x
+
+# Q10:
+y <- seq(6,55,4/7)
+length(y)
+y
+
+# Q11:
+a <- seq(1,10,0.5)
+class(a)
+
+# Q12:
+a <- seq(1,10)
+class(a)
+
+# Q13:
+class(1)
+class(1L)
+
+# Q14:
+x <- c("1","3","5")
+class(x)
+y <- as.integer(x)
+y
+class(y)
+
